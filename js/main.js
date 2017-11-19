4538f62acaaf1749596bae4e220a38a68056c7b1
Frank Flitton <fflitton@gmail.com>
2017-11-19 18:00:18 -0500

    Initial Commit
    Rough mock of interaction and body sections.

---
+++ js/main.js
@@ -0,0 +1,117 @@
+window.curSlide = void 0;
+window.clickC = 0
+window.featureImage = false;
+var initString = $( '.headlineContainer' ).html();
+
+$('body').scrollspy({ target: '#mainNav' })
+
+$(window).load(function() {
+    $('.sliderElement').flexslider({
+        animation: "fade",
+        controlNav: true,
+        animationLoop: false,
+        slideshow: false,
+        slideshowSpeed: 0,
+        animationSpeed: 0,
+        drag: true,
+        before: function() {},
+        after: function(slider) {
+            window.curSlide = slider.currentSlide;
+        },
+    });
+});
+
+$('body').click(function(event) {
+    setTimeout( function(){
+        if ( $('body').hasClass('featureImageOpen') ) {
+
+            $( '.featureImageContainer' ).fadeOut(500, function() {
+                $( '.featureImageContainer' ).addClass('disabled');
+                $( 'body' ).removeClass('featureImageOpen');
+                $( '.headlineContainer' ).fadeOut(500, function() {
+                    $( '.headlineContainer' ).html( initString );
+                }).fadeIn(500);
+            });
+
+            window.featureImage = false;
+        }
+    }, 100);
+    
+});
+
+$( '.featureImageContainer' ).addClass("disabled");
+
+$(".featurePoint").click(function(event) {
+    
+    var featureImageURL = 'img/homePage/Slider/featurePoints/' + event.target.id + '.jpg';
+    var htmlString = $( this ).html();
+    
+//    if (window.featureImage == false) {
+        
+        $('.featureImageContainer').fadeIn(500, function() {
+            $( 'div.featureImageContainer.disabled' ).removeClass('disabled');
+            $( '.imageTarget' ).attr('src', featureImageURL );
+        });
+
+        $( '.headlineContainer' ).fadeOut(500, function() {
+            $( '.headlineContainer' ).html( htmlString );
+        }).fadeIn(500).after(function() {
+            setTimeout(function() {
+                window.featureImage = true;
+                $( 'body' ).addClass('featureImageOpen');
+            }, 300);
+            
+        });
+        
+//    }
+});
+
+$(function() {
+    var pressed, pressX, pressY,
+        dragged,
+        offset = 10; // helps detect when the user really meant to drag
+
+    $(document)
+        .on('mousedown', '.sliderElement', function(e) {
+            pressX = e.pageX;
+            pressY = e.pageY;
+            pressed = true;
+        })
+        .on('mousemove', '.sliderElement', function(e) {
+            if (!pressed) return;
+            dragged = Math.abs(e.pageX - pressX) > offset;
+
+            if ((e.pageX - pressX) > offset) {
+                $('.flexslider').flexslider('next');
+            } else if ((e.pageX - pressX) < (0 - offset)) {
+                $('.flexslider').flexslider('prev');
+            }
+
+            pressX = e.pageX;
+
+        }).delay(200)
+        .on('mouseup', function() {
+            pressed = dragged = false;
+        });
+});
+
+
+$(document).ready(function () {
+    var sliderParallax = document.getElementsByClassName("sliderElement")[0];
+    var bgParallax = document.getElementsByClassName("backgroundImage")[0];
+    
+    sliderParallax.homePos = { x: sliderParallax.offsetLeft, y: sliderParallax.offsetTop };
+    bgParallax.homePos = { x: bgParallax.offsetLeft, y: bgParallax.offsetTop };
+    
+    $('.homeSlider').mousemove(function (e) {
+        parallax(e, sliderParallax, 100);
+        parallax(e, bgParallax, 20);
+    });
+});
+
+
+function parallax(e, target, layer) {
+    var x = target.homePos.x - (e.pageX - target.homePos.x) / layer;
+    var y = target.homePos.y - (e.pageY - target.homePos.y) / layer;
+    $(target).css({ top: y+10 ,left : x });
+};
---
+++ js/plugins.js
@@ -0,0 +1,37 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
